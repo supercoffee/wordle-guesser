7de66452fcd60f384a56c76be308305871b297b7
Benjamin Daschel <ben@lunarbyte.io>
2022-04-25 23:25:34 -0700

    complete filtering algorithm and wire up to UI

--- src/app/GameState.ts
+++ src/app/GameState.ts
@@ -14,30 +14,74 @@ export class GameState {
   removeGuess() {
     this.guesses.pop();
   }
+
+  listSuggestions(): string[] {
+
+    return this.wordlist.filter(testWord => {
+
+      return this.guesses.every(guess => guess.filterWord(testWord));
+    })
+
+  }
 }
 
 export class Guess {
 
   readonly guess: string[]
   private states: LetterState[];
+  private filters: WordFilter[];
 
   constructor(word: string) {
     this.guess = word.split('');
     this.states = this.guess.map(_ => LetterState.NONE);
+    this.filters = buildFiltersForGuess(this.guess, this.states);
   }
 
   toggleLetterState(index: number) {
     this.states[index] = (this.states[index] + 1) % (Object.keys(LetterState).length / 2);
+    this.filters = buildFiltersForGuess(this.guess, this.states);
   }
 
   letterStateAt(index: number) {
     return this.states[index];
   }
+
+  filterWord(testWord: string): boolean{
+    return this.filters.every(filter => filter(testWord));
+  }
+
 }
 
+function buildFiltersForGuess(guess: string[], states: LetterState[]): WordFilter[] {
+
+  const filterFunctions: WordFilterFactory[] = [
+    filterNone,
+    filterPosition,
+    filterCorrect
+  ];
+
+  return guess.map((letter, index) => {
+    const letterState = states[index];
+    return filterFunctions[letterState](letter, index);
+  });
+}
 export enum LetterState {
   NONE,
   MISPOSITIONED,
   CORRECT
 }
 
+interface WordFilter {
+  (word: string): boolean
+}
+type WordFilterFactory = ((letter: string, index:number) => WordFilter) | ((letter: string) =>WordFilter)
+
+function filterNone(letter: string): WordFilter {
+  return (word) => !word.includes(letter)
+}
+function filterPosition(letter: string, index: number): WordFilter {
+  return (word) => word.charAt(index) !== letter && word.includes(letter);
+}
+function filterCorrect(letter: string, index: number): WordFilter {
+  return (word) => word.charAt(index) === letter;
+}
--- src/app/suggestions/suggestions.component.ts
+++ src/app/suggestions/suggestions.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {GameState} from "../GameState";
 
 @Component({
   selector: 'app-suggestions',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuggestionsComponent implements OnInit {
 
+  @Input() gameState!: GameState;
   constructor() { }
 
   ngOnInit(): void {
@@ -14,10 +16,6 @@ export class SuggestionsComponent implements OnInit {
 
   get suggestions() {
 
-    return [
-      'qwert',
-      'asdfg',
-      'zxcvb'
-    ]
+    return this.gameState.listSuggestions().slice(0, 10);
   }
 }
